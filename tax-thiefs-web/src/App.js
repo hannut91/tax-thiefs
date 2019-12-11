6df2a7e526b38978d0d741ff74a165857e320099
hannut91 <hannut91@gmail.com>
2019-12-11 15:25:02 +0900

    Add Naver map
    Show map in main page using Naver map.
    Use react-naver-map module.
    
    See also
      - https://www.ncloud.com/product/applicationService/maps
      - https://github.com/zeakd/react-naver-maps

--- tax-thiefs-web/src/App.js
+++ tax-thiefs-web/src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+
+import { RenderAfterNavermapsLoaded, NaverMap } from 'react-naver-maps';
 
 function App() {
   return (
     <div className="App">
-      Hello world!
+      <RenderAfterNavermapsLoaded
+        ncpClientId={'qiq4esm1zw'}
+        error={<p>Maps Load Error</p>}
+        loading={<p>Maps Loading...</p>}
+      >
+        <NaverMap
+          mapDivId={'map'}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+          defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+          defaultZoom={10}
+        />
+      </RenderAfterNavermapsLoaded>
     </div>
   );
 }
