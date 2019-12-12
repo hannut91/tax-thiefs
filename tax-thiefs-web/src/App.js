abf2e461e2ef407095d2ac762c715c553735a9b9
hyeonhyejin <dev.hyejin04@gmail.com>
2019-12-12 15:49:25 +0900

    Update map location into my location
    1. 처음 사이트를 열면 나의 현재 위치를 찾는다.
    2. 지도를 나의 현재 위치로 이동시킨다.
    3. 에러가 발생하면 alert창을 띄운다.
    .

--- tax-thiefs-web/src/App.js
+++ tax-thiefs-web/src/App.js
@@ -1,42 +1,63 @@
-import React from 'react';
-import { RenderAfterNavermapsLoaded, NaverMap, Marker } from 'react-naver-maps';
+import React, { useEffect, useState } from "react";
+import { RenderAfterNavermapsLoaded, NaverMap, Marker } from "react-naver-maps";
 
-import './App.css';
+import "./App.css";
+import { getCurrentLocation } from './services/location.service'
 
-function App(props) {
-  const navermaps = window.naver.maps;
+function App() {
+  const coordinates = {
+    lat: 37.3595704,
+    lng: 127.105399
+  }
+  const [ref, setRef] = useState();
+
+  useEffect(() => {
+    if (!ref) {
+      return;
+    }
+
+    (async () => {
+      try {
+        const coordinates = await getCurrentLocation();
+        ref.panTo(new ref.props.navermaps.LatLng(coordinates.lat, coordinates.lng));
+      } catch (err) {
+        alert("에러가 발상했습니다. ", err);
+      }
+    })();
+  }, [ref]);
 
   const markers = [
     [37.360747, 127.105193], // 더 샵 스타파크
     [37.3595704, 127.105399], // 그린팩토리
-    [37.372616, 127.107392], // 정자동 파크뷰
-  ]
+    [37.372616, 127.107392] // 정자동 파크뷰
+  ];
+
   return (
     <div className="App">
       <RenderAfterNavermapsLoaded
-        ncpClientId={'qiq4esm1zw'}
+        ncpClientId={"qiq4esm1zw"}
         error={<p>Maps Load Error</p>}
         loading={<p>Maps Loading...</p>}
       >
         <NaverMap
-          mapDivId={'map'}
+          mapDivId={"map"}
           style={{
-            width: '100%',
-            height: '100%',
+            width: "100%",
+            height: "100%"
           }}
-          defaultCenter={{ lat: 37.3595704, lng: 127.105399 }}
+          naverRef={ref => setRef(ref)}
+          defaultCenter={{ lat: coordinates.lat, lng: coordinates.lng }}
           defaultZoom={10}
         >
-          {
-            markers.map(coordinates => (
-              <Marker
-                position={new navermaps.LatLng(coordinates[0], coordinates[1])}
-                onClick={() => {
-                  alert('여기는 네이버 입니다.')
-                }}
-              />
-            ))
-          }
+          {ref && markers.map((coordinates, index) => (
+            <Marker
+              position={new ref.props.navermaps.LatLng(coordinates[0], coordinates[1])}
+              key={index}
+              onClick={() => {
+                alert("여기는 네이버 입니다.");
+              }}
+            />
+          ))}
         </NaverMap>
       </RenderAfterNavermapsLoaded>
     </div>
